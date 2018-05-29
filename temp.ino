b32e4a34ab8ed52afd0551e822b01c886cb6c282
JediDuck96 <38986400+JediDuck96@users.noreply.github.com>
2018-05-29 12:26:16 -0400

    Add files via upload
    Code

---
+++ temp.ino
@@ -0,0 +1,55 @@
+/**
+ * Example for reading temperature and humidity
+ * using the DHT22 and ESP8266
+ *
+ * Copyright (c) 2016 Losant IoT. All rights reserved.
+ * https://www.losant.com
+ */
+
+#include "DHT.h"
+
+#define DHTPIN 4     // what digital pin the DHT22 is conected to
+#define DHTTYPE DHT22   // there are multiple kinds of DHT sensors
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.setTimeout(2000);
+
+  // Wait for serial to initialize.
+  while(!Serial) { }
+
+  Serial.println("Device Started");
+  Serial.println("-------------------------------------");
+  Serial.println("Running DHT!");
+  Serial.println("-------------------------------------");
+
+}
+
+int timeSinceLastRead = 0;
+void loop() {
+  Serial.println("hi")
+  if(timeSinceLastRead > 10000) {
+
+    // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+    float h = dht.readHumidity();
+    // Read temperature as Celsius (the default)
+    float t = dht.readTemperature();
+    
+    // Check if any reads failed and exit early (to try again).
+    if (isnan(h) || isnan(t)) {
+      Serial.println("Failed to read from DHT sensor!");
+      return;
+    }
+    Serial.print("Humidity: ");
+    Serial.print(h);
+    Serial.print(" %\t");
+    Serial.print("Temperature: ");
+    Serial.print(t);
+    Serial.println(" *C ");
+  }
+  
+  timeSinceLastRead += 10000;
+}
+
