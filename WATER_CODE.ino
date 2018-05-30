2a19138c7b6b55993ad1f045b412f03042840b56
JediDuck96 <38986400+JediDuck96@users.noreply.github.com>
2018-05-30 12:14:40 -0400

    Update WATER_CODE.ino

--- WATER_CODE.ino
+++ WATER_CODE.ino
@@ -1,10 +1,7 @@
 // constants won't change. Used here to set a pin number:
-const int ledPin =  LED_BUILTIN;// the number of the LED pin
 
-// Variables will change:
+// define pins relays will use 
 #define relay1 12
-#define relay2 13
-#define relay3 14
 unsigned long previousMillis = 0; 
 unsigned long pumppreviousMillis = 0; 
 int pump = 0;
@@ -14,36 +11,24 @@ const long interval = 10000;           // interval turn water on or off
 const long water = 30000;
 
 
-void setup() {
+void setup() { //declare pins and sets up relays
    pinMode(relay1, OUTPUT);
-   pinMode(relay2, OUTPUT);
-   pinMode(relay3, OUTPUT);
    digitalWrite(relay1, HIGH);
-   digitalWrite(relay2, HIGH);
-   digitalWrite(relay3, HIGH);
-
 }
 void loop() {
   
   unsigned long relayMillis = millis();
-  
-
   if (relayMillis - previousMillis >= interval) {
     // turn on pumps 
     previousMillis = relayMillis;
     digitalWrite(relay1, LOW);
-    digitalWrite(relay2, LOW);
-    digitalWrite(relay3, LOW);
     int pump = 1;
-  }
-    
-  if (pump == 1){// keep water on 
+  }  
+  if (pump == 1){// keep water on untill time is reached
     unsigned long pumpMillis = millis();
     if (pumpMillis - pumppreviousMillis >= water) {
       pumppreviousMillis = pumpMillis;
       digitalWrite(relay1, HIGH);
-      digitalWrite(relay2, HIGH);
-      digitalWrite(relay3, HIGH);
       int pump = 0;
       unsigned long pumpMillis = 0;
       }
